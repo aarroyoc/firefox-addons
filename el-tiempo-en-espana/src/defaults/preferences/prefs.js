6171690a9739b68aa3fa331ccd0c706bcc9392d1
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2013-03-28 04:37:28 +0100

    El Tiempo de Espana 1.0.1 SRC BIN

---
+++ el-tiempo-en-espana/src/defaults/preferences/prefs.js
@@ -0,0 +1,11 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+
+// These are here as an example only. Please remove them or rename them to
+// something useful.
+pref("extensions.el_tiempo_de_espa_a.boolpref", false);
+pref("extensions.el_tiempo_de_espa_a.intpref", 47010);
+pref("extensions.el_tiempo_de_espa_a.stringpref", "City");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.el_tiempo_de_espa_a.description", "chrome://el_tiempo_de_espa_a/locale/overlay.properties");
