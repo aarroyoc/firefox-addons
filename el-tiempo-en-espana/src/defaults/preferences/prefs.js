823f88ac80a886f08a7bfe9215fcc5577c08ab9e
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2013-06-03 20:00:52 +0200

    El Tiempo de España 2.1

--- el-tiempo-en-espana/src/defaults/preferences/prefs.js
+++ el-tiempo-en-espana/src/defaults/preferences/prefs.js
@@ -7,7 +7,7 @@ pref("extensions.el_tiempo_de_espa_a.boolpref", false);
 pref("extensions.el_tiempo_de_espa_a.intpref", 47010);
 pref("extensions.el_tiempo_de_espa_a.stringpref", "City");
 pref("extensions.el_tiempo_de_espa_a.numbercode",28079);
-pref("extensions.el_tiempo_de_espa_a.city","Madrid");
+pref("extensions.el_tiempo_de_espa_a.city","madrid");
 pref("extensions.el_tiempo_de_espa_a.iframeview",true);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
