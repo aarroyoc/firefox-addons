40e03f14ff535cc2ff41da4d967a4f9d26689b9a
Adrián Arroyo <adrian@divelonline.no-ip.org>
2013-10-07 21:17:15 +0200

    Little clean code

--- divel-notepad/src/lib/main.js
+++ divel-notepad/src/lib/main.js
@@ -3,6 +3,7 @@ var firefox=false;
 var thunderbird=false;
 var seamonkey=false;
 var fennec=false;
+var instantbird=false;
 const data = require("sdk/self").data;
 const notifications=require("notifications");
 
@@ -21,67 +22,47 @@ function guid() {
 
 function SaveData(title, body)
 {
-var { indexedDB }=require("sdk/indexed-db");
-var dbVersion=1.0;
+	var { indexedDB }=require("sdk/indexed-db");
+	var dbVersion=1.0;
 
-var request = indexedDB.open("QuickNotes", dbVersion),
-db,
-createObjectStore = function (dataBase) {
-// Create an objectStore
-    //console.log("Creating objectStore");
-    dataBase.createObjectStore("NOTE",{keyPath: 'id', autoIncrement: true});
-    
-},
- 
-putData = function () {
-    //console.log("Putting elephants in IndexedDB");
- 
-// Open a transaction to the database
-    //var transaction = db.transaction(["NOTE"], IDBTransaction.READ_WRITE);
-    var transaction = db.transaction(["NOTE"], "readwrite");
- 
-// Put the blob into the dabase
-    var put = transaction.objectStore("NOTE").put({
-    "id": guid(),
-    "title": title,
-    "body": body
-    });
-    put.onsuccess=function(){
-	notifications.notify({
-	  text: "Note added successfully",
-	  iconURL: data.url("save32.png")
-	});
-    }
-    //transaction.objectStore("NOTE").delete(1);
- 
-// Retrieve the file that was just stored
-    /*transaction.objectStore("NOTE").get("title").onsuccess = function (event) {
-    var imgFile = event.target.result;
-    if(imgFile)
-    {
-        console.log("Got elephant! " + imgFile.value.title);
-    }
-    };*/
-    //console.log("Consulting database");
-                       var transaction2 = db.transaction(["NOTE"], "readwrite");
-                    var objectStore2 = transaction2.objectStore("NOTE");
-                    var nothing="";
- 
-                    var request2 = objectStore2.openCursor();
-                    request2.onsuccess = function(evt) {  
-                        var cursor = evt.target.result;  
-                        if (cursor) {  
-                            nothing += "id: " + cursor.key + 
-                                        " with title " + cursor.value.title + " and body "+ cursor.value.body;
-                            //console.log(nothing);
-                            cursor.continue();  
-                        }  
-                        else {  
-                            //console.log("No more entries!");  
-                        }  
-                    };  
+	var request = indexedDB.open("QuickNotes", dbVersion),
+	db,
+	createObjectStore = function (dataBase) {
 
-};
+	    dataBase.createObjectStore("NOTE",{keyPath: 'id', autoIncrement: true});
+	    
+	},
+	 
+	putData = function () {
+
+		var transaction = db.transaction(["NOTE"], "readwrite");
+
+		var put = transaction.objectStore("NOTE").put({
+		"id": guid(),
+		"title": title,
+		"body": body
+		});
+		put.onsuccess=function(){
+		notifications.notify({
+		  text: "Note added successfully",
+		  iconURL: data.url("save32.png")
+		});
+		}
+	var transaction2 = db.transaction(["NOTE"], "readwrite");
+	var objectStore2 = transaction2.objectStore("NOTE");
+	var nothing="";
+	 
+	var request2 = objectStore2.openCursor();
+	request2.onsuccess = function(evt) {  
+		var cursor = evt.target.result;  
+		if (cursor) {  
+			nothing += "id: " + cursor.key + 
+			" with title " + cursor.value.title + " and body "+ cursor.value.body;
+			cursor.continue();  
+		}else{}  
+	};  
+
+	};
  
 request.onerror = function (event) {
 //console.log("Error creating/accessing IndexedDB database");
@@ -112,25 +93,13 @@ var dbVersion=1.0;
 var request = indexedDB.open("QuickNotes", dbVersion),
 db,
 createObjectStore = function (dataBase) {
-// Create an objectStore
-    //console.log("Creating objectStore");
     dataBase.createObjectStore("NOTE",{keyPath: 'id', autoIncrement: true});
     
 },
  
 putData = function () {
- 
-// Open a transaction to the database
-    //var transaction = db.transaction(["NOTE"], IDBTransaction.READ_WRITE);
     var transaction = db.transaction(["NOTE"], "readwrite");
  
-// Put the blob into the dabase
-    /*var put = transaction.objectStore("NOTE").put({
-    "id": guid(),
-    "title": title,
-    "body": body
-    });*/
-    //console.log("Deleting");
     var del=transaction.objectStore("NOTE").delete(id);
     del.onsuccess=function(){
 	notifications.notify({
@@ -138,17 +107,7 @@ putData = function () {
 		iconURL: data.url("open32.png")
 	});
     }
-    //console.log("Deleted");
- 
-// Retrieve the file that was just stored
-    /*transaction.objectStore("NOTE").get("title").onsuccess = function (event) {
-    var imgFile = event.target.result;
-    if(imgFile)
-    {
-        console.log("Got elephant! " + imgFile.value.title);
-    }
-    };*/
-    //console.log("Consulting database");
+
                        var transaction2 = db.transaction(["NOTE"], "readwrite");
                     var objectStore2 = transaction2.objectStore("NOTE");
                     var nothing="";
@@ -192,8 +151,6 @@ request.onsuccess = function (event) {
 }
 function QuickNote()
 {
-	//console.log("Starting QuickNOTE");
-	
 	var panel = require("sdk/panel");
  
 	var panelquick=panel.Panel({
@@ -201,149 +158,71 @@ function QuickNote()
 	  height: 500,
 	  contentURL: data.url("quicknote.html"),          
 	  contentScriptFile: data.url("quicknote.js")
-	  /*onMessage: function(message) {
-	    /*worker.port.emit("initQuickNote","NOTHING");
-	    worker.port.on("saveNote", function(title) {
-		console.log(title+"\n");
-		//Save the data in a IndexedDB
-		SaveData(title,"BODY TEST");
-	  });}
-	  },
-	  onShow: function()
-		{
-			this.postMessage("hello");
-		}*/
 	});
 	panelquick.port.emit("initQuickNote");
 	panelquick.port.on("saveNote",function(title,body){
-		//console.log("Title: "+title+" Body: "+body);
 		SaveData(title,body);
-
 	});
 	panelquick.show();
 	
 }
 function SendData(panelquick)
 {
-var { indexedDB }=require("sdk/indexed-db");
-var dbVersion=1.0;
+	var { indexedDB }=require("sdk/indexed-db");
+	var dbVersion=1.0;
+	var request = indexedDB.open("QuickNotes", dbVersion),
+	db,
+	createObjectStore = function (dataBase) {
+		dataBase.createObjectStore("NOTE",{keyPath: 'id', autoIncrement: true});
+	},
+	putData = function () {
+		var transaction = db.transaction(["NOTE"], "readwrite");
+		var transaction2 = db.transaction(["NOTE"], "readwrite");
+		var objectStore2 = transaction2.objectStore("NOTE");
+		var nothing="";
+ 
+		var request2 = objectStore2.openCursor();
+		request2.onsuccess = function(evt) {  
+			var cursor = evt.target.result;  
+				if (cursor) {
+					panelquick.port.emit("sendData",cursor.key,cursor.value.title,cursor.value.body);
+					cursor.continue();  
+				}else{}  
+		};
+	};
+	 
+	request.onerror = function (event) {
+	};
 
-var request = indexedDB.open("QuickNotes", dbVersion),
-db,
-createObjectStore = function (dataBase) {
-// Create an objectStore
-    //console.log("Creating objectStore");
-    dataBase.createObjectStore("NOTE",{keyPath: 'id', autoIncrement: true});
-    
-},
- 
-putData = function () {
-    //console.log("Putting elephants in IndexedDB");
- 
-// Open a transaction to the database
-    //var transaction = db.transaction(["NOTE"], IDBTransaction.READ_WRITE);
-    var transaction = db.transaction(["NOTE"], "readwrite");
- 
-// Put the blob into the dabase
-    /*var put = transaction.objectStore("NOTE").put({
-    "id": guid(),
-    "title": title,
-    "body": body
-    });*/
-    
-    //transaction.objectStore("NOTE").delete(id);
- 
-// Retrieve the file that was just stored
-    /*transaction.objectStore("NOTE").get("title").onsuccess = function (event) {
-    var imgFile = event.target.result;
-    if(imgFile)
-    {
-        console.log("Got elephant! " + imgFile.value.title);
-    }
-    };*/
-    //console.log("Consulting database");
-                       var transaction2 = db.transaction(["NOTE"], "readwrite");
-                    var objectStore2 = transaction2.objectStore("NOTE");
-                    var nothing="";
- 
-                    var request2 = objectStore2.openCursor();
-                    request2.onsuccess = function(evt) {  
-                        var cursor = evt.target.result;  
-                        if (cursor) {  
-                            //nothing += "id: " + cursor.key + 
-                            //           " with title " + cursor.value.title + " and body "+ cursor.value.body;
-                            panelquick.port.emit("sendData",cursor.key,cursor.value.title,cursor.value.body);
-                            cursor.continue();  
-                        }  
-                        else {  
-                            //console.log("No more entries!");  
-                        }  
-                    };  
-
-};
- 
-request.onerror = function (event) {
-//console.log("Error creating/accessing IndexedDB database");
-};
- 
-request.onsuccess = function (event) {
-    //console.log("Success creating/accessing IndexedDB database");
-    db = request.result;
-   
-    db.onerror = function (event) {
-        //console.log("Error creating/accessing IndexedDB database");
-    };
-     putData();
+	request.onsuccess = function (event) {
+    	db = request.result;
+    	db.onerror = function (event) {
+		};
+		putData();
     };
 
-// For future use. Currently only in latest Firefox versions
     request.onupgradeneeded = function (event) {
         createObjectStore(event.target.result);
-        //putData();
-        
     };
 }
 function ViewNote()
 {
 	if(firefox==true)
 	{
-		/*var addontab = require("sdk/addon-page");
-		
-		require("sdk/tabs").open(data.url("index.html"));*/
-		
-	var panel = require("sdk/panel");
- 
-	var panelquick=panel.Panel({
-	  width: 800,
-	  height: 600,
-	  contentURL: data.url("index.html"),          
-	  contentScriptFile: data.url("index.js")
-	  /*onMessage: function(message) {
-	    /*worker.port.emit("initQuickNote","NOTHING");
-	    worker.port.on("saveNote", function(title) {
-		console.log(title+"\n");
-		//Save the data in a IndexedDB
-		SaveData(title,"BODY TEST");
-	  });}
-	  },
-	  onShow: function()
-		{
-			this.postMessage("hello");
-		}*/
-	});
-	panelquick.port.emit("initScript");
-	//Read database and send TODO
-	SendData(panelquick);
-	
-	panelquick.port.on("delete",function(id,title,body){
-		//console.log("Title: "+title+" Body: "+body);
-		//Delete from DATABASE TODO
-		DeleteData(id);
-		
-	});
-	panelquick.show();
-
-
+		var panel = require("sdk/panel");
+	 
+		var panelquick=panel.Panel({
+		  width: 800,
+		  height: 600,
+		  contentURL: data.url("index.html"),          
+		  contentScriptFile: data.url("index.js")
+		});
+		panelquick.port.emit("initScript");
+		SendData(panelquick);
+		panelquick.port.on("delete",function(id,title,body){
+			DeleteData(id);	
+		});
+		panelquick.show();
 
 	}else{
 		require("sdk/tabs").open(data.url("index.html"));
@@ -354,15 +233,12 @@ function ViewNote()
 
 exports.main=function(options)
 {
-	//Check XUL App - define specific mode
 	if(options.loadReason=="install"){
-		require("tabs").open(data.url("welcome.html")); //Welcome HTML file
-		require("tabs").open("http://sites.google.com/site/divelmedia");
-		//Configurar con pagina XUL o HTML o Javascript
-	
+		require("tabs").open("http://adrianarroyocalle.github.io/firefox-addons/page/divel-notepad/welcome.html");
+		require("tabs").open("http://sites.google.com/site/divelmedia");	
 	}
 	if(options.loadReason=="upgrade"){
-		require("tabs").open(data.url("changelog.html")); //Changelog HTML file
+		require("tabs").open("http://adrianarroyocalle.github.io/firefox-addons/page/divel-notepad/changelog.html");
 		require("tabs").open("http://sites.google.com/site/divelmedia");
 	}
 	var xulapp=require("sdk/system/xul-app");
@@ -382,10 +258,8 @@ exports.main=function(options)
 	{
 		thunderbird=true;
 	}
-	//Create UI for every XUL App
 	if(firefox==true)
 	{
-		//Context menu for Mozilla Firefox
 		var mm = require("context-menu");
  		var quicknote = mm.Item({
  		 label: "Quick Note",
@@ -406,11 +280,6 @@ exports.main=function(options)
  		 }
 		});
 	}
-	/*
-	When the preferences bug dissapear, this part will be functional. Also use oldpackage.json in doc folder	
-	require("simple-prefs").on("review",function (){
-		require("tabs").open("http://addons.mozilla.org/en/firefox/addon/divel-notepad");
-	});*/
 	if(firefox==true)
 	{
 
@@ -418,10 +287,8 @@ exports.main=function(options)
 		var pluswidget=widget.Widget({
 			id: "divel-notepad-view",
 			label: "Quick Note",
-			contentURL: data.url("save32.png"), 	
-			//panel: pluspanel,
+			contentURL: data.url("save32.png"),
 			onClick: function() {
-				//pluspanel.contentURL=data.url("quicknote.html");
 				QuickNote();	
 			}
 		});
