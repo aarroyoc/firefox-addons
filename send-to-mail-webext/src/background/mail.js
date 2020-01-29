d199b1e516d07b6a0c16e5a1b4d0eb54e9a0ef53
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-01-29 23:20:19 +0100

    Simplify Send to Mail

--- send-to-mail-webext/src/background/mail.js
+++ send-to-mail-webext/src/background/mail.js
@@ -1,18 +1,18 @@
 function SendToMail(){
-chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-    var activeTab = arrayOfTabs[0];
-    var activeTabUrl = activeTab.url;
-    var activeTabTitle = activeTab.title;
-    chrome.tabs.create({
-		 url: "mailto:?to=&subject="+encodeURIComponent(activeTabTitle)+"&body="+encodeURIComponent(activeTabUrl)
-	},function(){
-		chrome.notifications.create({
-			"type": "basic",
-			"iconUrl": chrome.extension.getURL("icons/servermaildaemon.png"),
-			"title": "Send to Mail",
-			"message": "Opening mail handler"
-		  });	 
-	});
+	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+		var activeTab = arrayOfTabs[0];
+		var activeTabUrl = activeTab.url;
+		var activeTabTitle = activeTab.title;
+		chrome.tabs.create({
+			url: "mailto:?to=&subject="+encodeURIComponent(activeTabTitle)+"&body="+encodeURIComponent(activeTabUrl)
+		},function(){
+			chrome.notifications.create({
+				"type": "basic",
+				"iconUrl": chrome.extension.getURL("icons/servermaildaemon.png"),
+				"title": "Send to Mail",
+				"message": "Opening mail handler"
+			});	 
+		});
 });
 }
 
