a86dd74c371a205ab724f1aa5b6c87f4642f598a
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2021-03-21 21:27:41 +0100

    Send to Mail 2.0.3

--- send-to-mail-webext/src/background/mail.js
+++ send-to-mail-webext/src/background/mail.js
@@ -4,7 +4,7 @@ function SendToMail(){
 		var activeTabUrl = activeTab.url;
 		var activeTabTitle = activeTab.title;
 		chrome.tabs.create({
-			url: "mailto:?to=&subject="+encodeURIComponent(activeTabTitle)+"&body="+encodeURIComponent(activeTabUrl)
+			url: "mailto:?subject="+encodeURIComponent(activeTabTitle)+"&body="+encodeURIComponent(activeTabUrl)
 		},function(){
 			chrome.notifications.create({
 				"type": "basic",
